95a888ad8b309f8621a4f4b737028718cdab7aaa
jproyo <juanpablo.royo@gmail.com>
2013-04-23 13:53:33 -0300

    [#OPINST-8: Change user git]

--- manifests/install.pp
+++ manifests/install.pp
@@ -32,9 +32,4 @@ class git::install(
     package{$git::params::gui_package: ensure => absent}
   }
 
-  $root_name    = "root on ${::fqdn}"
-  $root_email   = "root@${::fqdn}"
-
-  git::user{'root':}
-
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -3,16 +3,12 @@
 # Configure how the puppet git module behaves
 
 class git::params {
-
-  case $::operatingsystem {
-    'CentOS','Ubuntu', 'Debian', 'Amazon' :{
-      $package      = 'git'
-      $svn_package  = 'git-svn'
-      $gui_package  = 'git-gui'
-      $bin          = '/usr/bin/git'
-    }
-    default:{
-      warning("git not configured for ${::operatingsystem} on ${::fqdn}")
-    }
+  $svn_package  = 'git-svn'
+  $gui_package  = 'git-gui'
+  $bin          = '/usr/bin/git'
+  if $::operatingsystem =~ /^(Debian|Ubuntu)$/ and versioncmp($::operatingsystemrelease, "12") < 0 {
+    $package = 'git-core'
+  }else{
+     $package = 'git'
   }
 }
